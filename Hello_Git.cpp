f5e876ac155e673d715735a24786f29fad0b4f7a
By41k <tygatyn228@gmail.com>
2023-12-27 23:49:53 +0300

    task 5

--- Hello_Git.cpp
+++ Hello_Git.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 using namespace std;
+void print()
+{
+    cout << "Hello, Git!!!!\n";
+    return;
+}
+void print_name()
+{
+    cout << "The work was performed by a student Bychinskii\n";
+    return;
+}
 int main()
 {
-    cout << "Hello, Git!";
+    print();
+    print_name();
     return 0;
 }
